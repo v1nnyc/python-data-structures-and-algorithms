c2e13f65704a28f58d3618a436b4679408375c7f
Vincent <vcannall@gmail.com>
2023-01-03 16:43:18 -0500

    ch 4.1 recursive ruler

---
+++ recursion/ruler.py
@@ -0,0 +1,35 @@
+# ruler.py
+
+
+def draw_line(tick_length, tick_label=" "):
+    """Draw one line with given tick length (followed by optional label)."""
+    line = "-" * tick_length
+    if tick_label:
+        line += " " + tick_label
+    print(line)
+
+
+def draw_interval(center_length):
+    """Draw tick interval based upon a central tick length."""
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
+
+
+def draw_ruler(num_inches, major_length):
+    """Draw English ruler with given number of inches, major tick length."""
+    draw_line(major_length, "0")
+    for j in range(1, 1 + num_inches):
+        draw_interval(major_length - 1)
+        draw_line(major_length, str(j))
+
+
+def main():
+    # This is the main function of the script
+    draw_ruler(3, 4)
+
+
+if __name__ == "__main__":
+    # This block of code will only be executed if you run the script from the terminal
+    main()
